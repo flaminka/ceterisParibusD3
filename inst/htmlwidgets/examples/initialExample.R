0f42740cebdcd9c3cc853902f823f62de5ccb604
Ewa Baranowska <ewa.baranowska4@wp.pl>
2018-09-27 08:23:44 +0200

    Added examples

--- inst/htmlwidgets/examples/initialExample.R
+++ inst/htmlwidgets/examples/initialExample.R
@@ -2,7 +2,49 @@ library("DALEX")
 library("randomForest")
 library("ceterisParibus")
 library(ceterisParibusD3)
-set.seed(59)
+
+
+######### first example: one variable, 2 obs, 1 model
+
+apartments_rf_model <- randomForest(m2.price ~ construction.year + surface + floor +
+                                      no.rooms + district, 
+                                    data = apartments)
+
+explainer_rf <- explain(apartments_rf_model,
+                        data = apartmentsTest[,2:6], 
+                        y = apartmentsTest$m2.price)
+
+apartments_A <- apartmentsTest[c(958, 353),]
+
+cp_rf_A <- ceteris_paribus(explainer_rf, apartments_A, y = apartments_A$m2.price)
+
+#plot(cp_rf_A, selected_variables = c("surface"), show_observations = FALSE)
+ceterisParibusD3(cp_rf_A, selected_variables = c("surface"),  width = 500, height = 300)
+
+
+######### second example: one variable, 2 obs, 2 models
+
+apartments_lm_model <- lm(m2.price ~ construction.year + surface + floor + 
+                            no.rooms + district, data = apartments)
+
+apartments_rf_model <-randomForest(m2.price ~ construction.year + surface + floor + 
+                                     no.rooms + district, data = apartments)
+
+explainer_rf <- explain(apartments_rf_model, 
+                        data = apartmentsTest[,2:6], y = apartmentsTest$m2.price)
+explainer_lm <- explain(apartments_lm_model, 
+                        data = apartmentsTest[,2:6], y = apartmentsTest$m2.price)
+
+new_apartment <- apartmentsTest[c(1,2), ]
+
+cp_rf <- ceteris_paribus(explainer_rf, observation = new_apartment)
+cp_lm <- ceteris_paribus(explainer_lm, observation = new_apartment)
+
+#plot(cp_rf, cp_lm, selected_variables = c("surface"), color = '_label_', show_observations = FALSE)
+
+ceterisParibusD3(cp_rf, cp_lm, selected_variables = c("surface"), color = '_label_',  width = 500, height = 300)
+
+######### third example: one variable, 2 obs, 1 model, 3 classes
 
 model <- randomForest(status ~ gender + age + hours + evaluation + salary, data = HR)
 
@@ -24,7 +66,27 @@ cp_rf1 <- ceteris_paribus(explainer_rf_fired, HR[1,])
 cp_rf2 <- ceteris_paribus(explainer_rf_ok, HR[1,])
 cp_rf3 <- ceteris_paribus(explainer_rf_promoted, HR[1,])
 
+#plot(cp_rf1, cp_rf2, cp_rf3, color="_label_", selected_variables = c('age'), show_observations = FALSE)
+
 ceterisParibusD3(cp_rf1, cp_rf2, cp_rf3, selected_variables = c('age'), color = "_label_",
                  width = 500, height = 300)
 
+######### forth example: two variables, many observations, 1 model
 
+apartments_rf_model <- randomForest(m2.price ~ construction.year + surface + floor +
+                                      no.rooms + district, 
+                                    data = apartments)
+
+explainer_rf <- explain(apartments_rf_model,
+                        data = apartmentsTest[,2:6], 
+                        y = apartmentsTest$m2.price)
+
+
+apartments_B <- select_neighbours(apartmentsTest, apartmentsTest[958,], n = 15)
+cp_rf_B <- ceteris_paribus(explainer_rf, apartments_B, y = apartments_B$m2.price)
+
+
+#plot(cp_rf_B,  show_observations = FALSE, selected_variables = c("surface","construction.year"), color='_label_')
+
+ceterisParibusD3(cp_rf_B, selected_variables = c("surface","construction.year"), color = "_label_",
+                 width = 500, height = 300)
